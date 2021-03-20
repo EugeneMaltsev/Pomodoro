a756e0d2414593b1a5362275a27a1d1a4829bd09
Eugene Maltsev <no.one.left.disappointed@gmail.com>
2021-03-20 17:01:54 +0200

    Pomodoro prototype with basic functional

--- Pomodoro/ViewController.swift
+++ Pomodoro/ViewController.swift
@@ -7,13 +7,89 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var timer = Timer()
+    var timerBreak = Timer()
+    var count = 1500
+    var countBreak = 300
+
+    var work = false
+    
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var stopWatchAction: UILabel!
+    
+    
+    @IBAction func startButton(_ sender: UIButton) {
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(countTimer),
+                                     userInfo: nil, repeats: true)
+        startButton.isHidden = true
+        pauseButton.isHidden = false
+        stopButton.isHidden = true
+    }
+    
+    @IBAction func pauseButton(_ sender: UIButton) {
+        timer.invalidate()
+        startButton.isHidden = false
+        pauseButton.isHidden = true
+        stopButton.isHidden = false
+    }
+    
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        reset()
+        stopButton.isHidden = true
+        startButton.isHidden = false
+        pauseButton.isHidden = true
     }
 
+    func reset() {
+        timer.invalidate()
+        count = 1500
+        countBreak = 300
+        stopWatchAction.text = formatTime(time: count)
+    }
+    
+    @objc func countTimer() {
+        
+        if count > -1 {
+            work = true
+            stopWatchAction.text = formatTime(time: count)
+            count -= 1
+        } else {
+            work = false
+        }
+        
+        if work == false {
+            if countBreak > -1 {
+                stopWatchAction.text = formatTime(time: countBreak)
+                countBreak -= 1
+            } else {
+                reset()
+                startButton(startButton)
+            }
+        }
+    }
+    
+    func formatTime(time: Int) -> String {
+        
+        let minutes = Int(time) / 60 % 60
+        let seconds = Int(time) % 60
+
+        return String(format: "%02i:%02i", minutes, seconds)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
 
 }
 
