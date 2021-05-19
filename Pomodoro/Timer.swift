943c85845fb1f1cb89ac39bc7ccfc9182ccad770
Eugene Maltsev <no.one.left.disappointed@gmail.com>
2021-05-19 21:15:22 +0300

    Work and Break cycle are fixed

--- Pomodoro/Timer.swift
+++ Pomodoro/Timer.swift
@@ -7,45 +7,128 @@
 
 import Foundation
 
+public protocol PomodoroDelegate {
+    func workTime(_ : TimeInterval)
+    func breakTime(_: TimeInterval)
+}
+
+
 public class PomodoroModel {
     
     public typealias TimeInterval = UInt
     public typealias TimeHandler = (TimeInterval) -> Void
     
+    public var delegate: PomodoroDelegate?
+    
     public var workTimeHandler: TimeHandler?
     public var breakTimeHandler: TimeHandler?
     
-    init(workTimeInterval: TimeInterval = 1500, breakTimeInterval: TimeInterval = 300) {
+    init( workTimeInterval: TimeInterval = 10,
+          breakTimeInterval: TimeInterval = 7) {
+        
         self.workTimeInterval = workTimeInterval
         self.breakTimeInterval = breakTimeInterval
     }
 
+    
+    
+    
+//    func startWorkTimer() {
+//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in self?.workTimerLogic() }
+//    }
+//
+//    func workTimerLogic() -> Int {
+//        return 0
+//    }
+    
+    
+    
     func start() {
         assert(timer == nil)
-
+        
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
             guard let self = self else { return }
             guard self.timer == timer else { return }
             
-            if self.work {
-                assert(self.workTimeInterval > 0)
-                self.workTimeInterval -= 1
-                self.workTimeHandler?(self.workTimeInterval)
-                if self.workTimeInterval == 0 {
-                    self.work = false
-                }
-            } else {
-                assert(self.breakTimeInterval > 0)
-                self.breakTimeInterval -= 1
-                self.breakTimeHandler?(self.breakTimeInterval)
-                if self.breakTimeInterval == 0 {
-                    timer.invalidate()
-                    self.timer = nil
-                }
+            
+            assert(self.workTimeInterval > 0)
+            self.workTimeInterval -= 1
+            self.workTimeHandler?(self.workTimeInterval)
+            if self.workTimeInterval == 0 {
+                timer.invalidate()
+                self.timer = nil
+                self.workTimeInterval = 10
+                self.breakStart()
             }
         }
     }
+    
+    func breakStart() {
+        assert(timer == nil)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+            guard let self = self else { return }
+            guard self.timer == timer else { return }
+            
+            assert(self.breakTimeInterval > 0)
+            self.breakTimeInterval -= 1
+            self.breakTimeHandler?(self.breakTimeInterval)
+            if self.breakTimeInterval == 0 {
+                timer.invalidate()
+                self.timer = nil
+                self.breakTimeInterval = 7
+                self.start()
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+//
+//    func start() {
+//        assert(timer == nil)
+//
+//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+//            guard let self = self else { return }
+//            guard self.timer == timer else { return }
+//
+//            if self.work {
+//                assert(self.workTimeInterval > 0)
+//                self.workTimeInterval -= 1
+//                self.workTimeHandler?(self.workTimeInterval)
+//                if self.workTimeInterval == 0 {
+//                    self.work = false
+//                }
+//            } else {
+//                assert(self.breakTimeInterval > 0)
+//                self.breakTimeInterval -= 1
+//                self.breakTimeHandler?(self.breakTimeInterval)
+//                if self.breakTimeInterval == 0 {
+//                    timer.invalidate()
+//                    self.timer = nil
+//                }
+//            }
+//        }
+//    }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     private var timer: Timer?
     private var workTimeInterval: TimeInterval
     private var breakTimeInterval: TimeInterval
@@ -61,3 +144,4 @@ public class PomodoroModel {
 //        timer.invalidate()
 //    }
 }
+
